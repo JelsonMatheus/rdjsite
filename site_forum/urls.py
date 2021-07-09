3d81c8f8f72bb2d4a7fa10b726ec9b786d41cb84
Jelson Matheus <jelsonmatheus200f@gmail.com>
2021-07-09 20:11:39 -0300

    Adiciona tela de login

--- site_forum/urls.py
+++ site_forum/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'forum'
 
 urlpatterns = [
-    path('', views.redirect_to_forum_view, name='forum_index'),
-    path('accounts/logout/', views.logout_view, name='forum_logout'),
+    path('', views.redirect_to_forum_view, name='index'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 
-    path('foruns/<slug:slug>/', views.TopicoListView.as_view(), name='forum_page'),
-    path('foruns/<slug:slug>/<int:topico_id>/', views.TopicoListView.as_view(), name='forum_topico'),
+    path('foruns/<slug:slug>/', views.TopicoListView.as_view(), name='page'),
+    path('foruns/<slug:slug>/<int:topico_id>/', views.TopicoListView.as_view(), name='topico'),
 ]
